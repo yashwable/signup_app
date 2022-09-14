d66d1efec31efb7441dee2139284bd165aebf5f0
yash <yashwable12@gmail.com>
2022-09-14 17:42:37 +0530

    function added

--- signupfront/src/App.jsx
+++ signupfront/src/App.jsx
@@ -1,108 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import axios from 'axios'
 
-class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            fullName : "",
-            username : "",
-            email : "",
-            password : ""
-        }
-        this.changeFullName = this.changeFullName.bind(this)
-        this.changeEmail = this.changeEmail.bind(this)
-        this.changeUsername = this.changeUsername.bind(this)
-        this.changePassword = this.changePassword.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
+function App(){
+    
+    const [inputs,setInputs] = useState({});
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        
     }
 
-    changeFullName(event){
-        this.setState({
-            fullName : event.target.value
-        })
-    }
-    changeUsername(event){
-        this.setState({
-            username : event.target.value
-        })
-    }
-    changeEmail(event){
-        this.setState({
-            email : event.target.value
-        })
-    }
-    changePassword(event){
-        this.setState({
-            password : event.target.value
-        })
+    const handleChange = (event)=> {
+        const name = event.target.name 
+        const value = event.target.value
+        setInputs(values => ({...values,[name]:value}))
     }
 
-    onSubmit(event){
-        event.preventDefault()
+    return(
+        
 
-        const registered = {
-            fullName : this.state.fullName,
-            username : this.state.username,
-            email : this.state.email,
-            password : this.state.password
-        }
+        <div className='form-div'>
+            <form className='form' onSubmit={handleSubmit}>
+            <label>Input your name here 
+                    <input 
+                    type='name'
+                    name= 'name'
+                    value={inputs.name || ""}
+                    onChange = {handleChange}  />
+                </label>
+                <label>Input your username
+                    <input 
+                    type= "text" 
+                    name='username'
+                    value={inputs.username || ""}
+                    onChange = {handleChange} />
+                </label>
+                <label>Input your email here 
+                    <input 
+                    type='email'
+                    name= 'email'
+                    value={inputs.email || ""}
+                    onChange = {handleChange}  />
+                </label>
+                <label>Input your password here 
+                    <input 
+                    type='password'
+                    name= 'password'
+                    value={inputs.password || ""}
+                    onChange = {handleChange}  />
+                </label>
+                <input type="submit"/>
+                
 
-        axios.post('http://localhost:4000/app/signup',registered)
-        .then(response => console.log(response.data))
-
-        this.setState({
-            fullName : '',
-            username : '',
-            email : '',
-            password : ''
-        })
-    }
-
-    render() { 
-        return (
-            <div>
-                <div className='container'>
-                    <div className='form-div'>
-                        <form onSubmit={this.onSubmit}>
-                            <input type= 'text' 
-                            placeholder = 'Full Name'
-                            onChange = {this.changeFullName}
-                            value = {this.state.fullName}
-                            className = 'form-control form-group'
-                            />
-
-                            <input type= 'text' 
-                            placeholder = 'Username'
-                            onChange = {this.changeUsername}
-                            value = {this.state.username}
-                            className = 'form-control form-group'
-                            />
-
-                            <input type= 'text' 
-                            placeholder = 'E-mail'
-                            onChange = {this.changeEmail}
-                            value = {this.state.email}
-                            className = 'form-control form-group'
-                            />
-
-                            <input type= 'password' 
-                            placeholder = 'password'
-                            onChange = {this.changePassword}
-                            value = {this.state.password}
-                            className = 'form-control form-group'
-                            />
-
-                            <input type='submit' className = 'btn btn-danger btn-block ' value = 'Submit'  />
-
-
-                        </form>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+            </form>
+        </div>
+    )
 }
+
  
 export default App;
